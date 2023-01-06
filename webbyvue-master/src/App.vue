0a37c7195a213b0a8961fad1aab7486c008ebccc
Amandine <90777055+AmandineLec@users.noreply.github.com>
2023-01-06 17:08:20 +0100

    Add files via upload

---
+++ webbyvue-master/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="foot">
+    <Header></Header>
+    <main id="main">
+      <router-view></router-view>
+    </main>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Footer,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.foot {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+</style>
+  
+
